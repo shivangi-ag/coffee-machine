54a6cee0f870b8c425f95bb12468e756de0353c8
Shivangi Agrawal <shivangi.agrawal@myntra.com>
2020-08-02 16:09:45 +0530

    adding functional test case

--- src/test/java/CoffeeMachineTest.java
+++ src/test/java/CoffeeMachineTest.java
@@ -4,10 +4,12 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.net.URL;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
 import model.CreateBeverageResult;
 import model.Input;
 import model.Machine;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -23,10 +25,61 @@ class CoffeeMachineTest {
 
   @Test
   public void testBeveragePreparation() throws ExecutionException, InterruptedException {
+    Map<String, Integer> originalInventory = machine.getIngredients();
+    Map<String, Map<String, Integer>> beverageDetails = machine.getBeverages();
+
     List<CreateBeverageResult> results = coffeeMachine.run(machine);
 
-    System.out.println("done");
+    updateInventoryAfterBeveragesPrepared(originalInventory, beverageDetails, results);
 
+    boolean isAnyIngredientMissing = false;
+    boolean isAnyBeverageNotPrepared = false;
+
+    for (CreateBeverageResult result : results) {
+      if (!result.isSuccessful()) {
+        isAnyBeverageNotPrepared = true;
+        String beverageName = result.getBeverageName();
+        Map<String, Integer> requiredIngredients = beverageDetails.get(beverageName);
+
+        for (Map.Entry<String, Integer> ingredient : requiredIngredients.entrySet()) {
+          String ingredientName = ingredient.getKey();
+          Integer availableQuantity = originalInventory.getOrDefault(ingredientName, 0);
+          Integer requiredQuantity = requiredIngredients.get(ingredientName);
+
+          if (availableQuantity < requiredQuantity) {
+            isAnyIngredientMissing = true;
+            break;
+          }
+        }
+      }
+    }
+
+    if (isAnyBeverageNotPrepared) {
+      Assertions.assertTrue(isAnyIngredientMissing);
+    }
+
+    if (!isAnyBeverageNotPrepared) {
+      Assertions.assertFalse(isAnyIngredientMissing);
+    }
+
+  }
+
+  private void updateInventoryAfterBeveragesPrepared(Map<String, Integer> originalInventory,
+      Map<String, Map<String, Integer>> beverageDetails, List<CreateBeverageResult> results) {
+    for (CreateBeverageResult result : results) {
+      if (result.isSuccessful()) {
+        String beverageName = result.getBeverageName();
+        Map<String, Integer> requiredIngredients = beverageDetails.get(beverageName);
+
+        for (Map.Entry<String, Integer> ingredient : requiredIngredients.entrySet()) {
+          String ingredientName = ingredient.getKey();
+          Integer availableQuantity = originalInventory.get(ingredientName);
+          Integer requiredQuantity = requiredIngredients.get(ingredientName);
+
+          originalInventory.put(ingredientName, availableQuantity - requiredQuantity);
+        }
+      }
+    }
   }
 
   private static Input getInput() throws FileNotFoundException {
